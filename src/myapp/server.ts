a451cd01dc18893a6d829496a98a11654306f5c5
BALSS <balss@debian>
2022-10-24 06:04:36 -0400

    debut

---
+++ src/myapp/server.ts
@@ -0,0 +1,13 @@
+const express = require ('express');
+
+const app = express();
+const port = 3000
+
+app.get('/', (req: any, res: { send: (arg0: string) => void; }) => {
+    res.send('Hello word')
+})
+
+// app.listen(3000, console.log("server started on port 3000"));
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
