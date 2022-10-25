0628931158af454a928e82a4114baaa51fc73c6e
BALSS <balss@debian>
2022-10-25 03:39:19 -0400

    fin

--- src/myapp/server.ts
+++ src/myapp/server.ts
@@ -8,6 +8,14 @@ app.get('/', (req: any, res: { send: (arg0: string) => void; }) => {
 })
 
 // app.listen(3000, console.log("server started on port 3000"));
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+//   })
+
+  app.post('/', (req: any, res: { send: (arg0: string) => void; }) => {
+    res.send('I am a POST request')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
   })
